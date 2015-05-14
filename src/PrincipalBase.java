1465b91f16ca9ea764eaf94657945347e7590609
Juan Garzon <jpgarzon@javeriana.edu.co>
2015-05-14 12:10:07 -0500

    primero desde workspace 1

---
+++ src/PrincipalBase.java
@@ -0,0 +1,13 @@
+
+public class PrincipalBase {
+	//Este es el codigo base desde el workspace1
+	
+	//1. Se crea el proyecto normal
+	//2. Desde Team se selecciona share
+	//3.Se selecciona crear un repositoria en el folder sup
+	//4. se crea
+	//5. se selecciona commit
+	
+	
+
+}
