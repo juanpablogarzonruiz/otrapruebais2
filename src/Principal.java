0fc1e08461cf114f32e646b617b4d8114e4ef931
Juan Garzon <jpgarzon@javeriana.edu.co>
2015-05-14 07:16:35 -0500

    buen d

--- src/Principal.java
+++ src/Principal.java
@@ -15,4 +15,20 @@ public class Principal {
 	  
 	  
 	}
+	
+	public void otrometodo()
+	{
+		int i=1;
+		
+		i=i+1;
+		i=i*4/2;
+	}
+	
+	
+	//para sincronizar de otro hilo toca por avanzadas y seleccionar el otro btranch, depsues el merge
+	public void ejemplo()
+	{
+		String inf="";
+		
+	}
 }
